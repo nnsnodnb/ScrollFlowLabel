9deb825405d01dcc6618a1598518cd558170d967
nnsnodnb <nnsnodnb@gmail.com>
2019-07-15 15:05:55 +0900

    Initial commit

---
+++ ScrollFlowLabel/ScrollFlowLabel.h
@@ -0,0 +1,19 @@
+//
+//  ScrollFlowLabel.h
+//  ScrollFlowLabel
+//
+//  Created by Yuya Oka on 2019/07/15.
+//  Copyright © 2019 Yuya Oka. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ScrollFlowLabel.
+FOUNDATION_EXPORT double ScrollFlowLabelVersionNumber;
+
+//! Project version string for ScrollFlowLabel.
+FOUNDATION_EXPORT const unsigned char ScrollFlowLabelVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ScrollFlowLabel/PublicHeader.h>
+
+
---
+++ ScrollFlowLabel/ScrollFlowLabel.swift
@@ -0,0 +1,13 @@
+//
+//  ScrollFlowLabel.swift
+//  ScrollFlowLabel
+//
+//  Created by Yuya Oka on 2019/07/15.
+//  Copyright © 2019 Yuya Oka. All rights reserved.
+//
+
+import UIKit
+
+public class ScrollFlowLabel: UIView {
+
+}
